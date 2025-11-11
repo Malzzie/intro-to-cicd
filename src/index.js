ba71aaf4c916c781f015aea4f0c91387fed04aae
Malibongwe <malzsoj@gmail.com>
2025-11-11 20:43:27 +0200

    trivial edit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // function to greet a user by name
 function sayHi(name) {
-  return `Hello there ${name}`
+  // return the greeting message
+  return `Hello ${name}`
 }
-
+// export the function for use in other files
 module.exports = sayHi
